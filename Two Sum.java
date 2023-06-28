913fc4e44f5eca2bedd69bab0f80fe2a74026d4b
Gada Suriya Narayana <116232067+SuriyaNarayana6765@users.noreply.github.com>
2023-06-28 15:57:21 +0530

    Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java. util.scanner;
 
 public class TwoSum {
 
@@ -34,28 +34,14 @@ public class TwoSum {
         arr = mergedArray;
     }
 
-    public static List<List<Integer>> findDoublePairs(int[] arr, int target) {
-        List<List<Integer>> pairs = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                for (int k = j + 1; k < arr.length; k++) {
-                    if (arr[i] + arr[j] + arr[k] == target) {
-                        pairs.add(Arrays.asList(arr[i], arr[j], arr[k]));
-                    }
-                }
-            }
-        }
-        return pairs;
-    }
-
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 2, -4, -6, -2, 8};
         int target = 4;
-        List<List<Integer>> pairs = findPairs(arr, target);
-        System.out.println("First Combination For “4” : " + pairs);
+        List<List<Integer>> pairs = find pairs(arr, target);
+        System. out.println("First Combination For “4” : " + pairs);
         mergeArray(arr);
-        int doubleTarget = 2 * target;
-        List<List<Integer>> doublePairs = findDoublePairs(arr, doubleTarget);
-        System.out.println("Second Combination For “8” : " + doublePairs);
+        int double target = 2 * target;
+        List<List<Integer>> double pairs = findDoublePairs(arr, double target);
+        System. out.println("Second Combination For “8” : " + double pairs);
     }
 }
